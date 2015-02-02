b96da888628a6b037d55adb241e367b7ff361970
satcy <satoshi.horii@gmail.com>
2015-02-02 19:12:56 +0900

    first commit

---
+++ example/src/ofApp.cpp
@@ -0,0 +1,86 @@
+#include "ofApp.h"
+
+#include "ofxGPC.h"
+
+ofPath path;
+ofPath path2;
+
+vector<ofPolyline> lines;
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    path.moveTo(0,0);
+    path.lineTo(300, 300);
+    path.lineTo(300, 400);
+    path.close();
+    
+    path2.moveTo(100,50);
+    path2.lineTo(200, 250);
+    path2.lineTo(150, 500);
+    path2.close();
+    
+    lines = ofxGPC::getPlygonClip(GPC_UNION, path, path2);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    path.draw();
+    path2.draw();
+    
+    ofPushMatrix();
+    ofTranslate(400, 0);
+    for ( auto p : lines ) {
+        p.draw();
+    }
+    ofPopMatrix();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ src/ofxGPC.h
@@ -0,0 +1,78 @@
+//
+//  ofxGPC.h
+//
+//  Created by satcy on 2/2/15.
+//
+//
+
+#pragma once
+
+#include "ofMain.h"
+#include "gpc.c"
+
+namespace ofx { namespace gpc {
+    typedef gpc_op op;
+    
+    inline gpc_polygon ofToGpc(ofPath p){
+        vector<ofPolyline> lines = p.getOutline();
+        gpc_polygon a;
+        a.num_contours = lines.size();
+        a.hole = NULL;
+        a.contour = NULL;
+        
+        MALLOC(a.hole, a.num_contours * sizeof(int), "contour hole", int);
+        MALLOC(a.contour, a.num_contours * sizeof(gpc_vertex_list), "contour", gpc_vertex_list);
+        
+        for ( int i=0; i<lines.size(); i++ ) {
+            ofPolyline pl = lines[i];
+            vector<ofPoint> verts = pl.getVertices();
+            int num = verts.size();
+            
+            gpc_vertex_list g_verts;
+            g_verts.num_vertices = num;
+            MALLOC(g_verts.vertex, num * sizeof(gpc_vertex), "vertex", gpc_vertex);
+            
+            for ( int j=0; j<num; j++ ) {
+                gpc_vertex g_vec;
+                g_vec.x = verts[j].x;
+                g_vec.y = verts[j].y;
+                
+                g_verts.vertex[j] = g_vec;
+            }
+            
+            a.hole[i] = FALSE;
+            a.contour[i] = g_verts;
+            
+        }
+        
+        return a;
+    }
+    
+    inline vector<ofPolyline> gpcToOf(gpc_polygon p){
+        vector<ofPolyline> res;
+        for ( int i=0; i<p.num_contours; i++ ) {
+            ofPolyline pl;
+            for ( int j=0; j<p.contour[i].num_vertices; j++ ) {
+                pl.addVertex(p.contour[i].vertex[j].x, p.contour[i].vertex[j].y);
+            }
+            pl.close();
+            res.push_back(pl);
+        }
+        return res;
+    }
+    
+    inline vector<ofPolyline> getPlygonClip(op operation, ofPath sub, ofPath clip){
+        gpc_polygon a = ofToGpc(sub);
+        gpc_polygon b = ofToGpc(clip);
+        
+        gpc_polygon res;
+        
+        gpc_polygon_clip(operation, &a, &b, &res);
+        
+        return gpcToOf(res);
+    }
+    
+    
+}}
+
+namespace ofxGPC = ofx::gpc;
